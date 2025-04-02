95f5d2ed11dbf280b84742376162913ac1d1816f
vbondoux <vbondoux@gmail.com>
2025-04-02 10:14:27 +0200

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import GraphPage from './GraphPage';
+
+const App = () => {
+  return (
+    <div>
+      <h1>📊 Mailjet Graph Live</h1>
+      <GraphPage />
+    </div>
+  );
+};
+
+export default App;
